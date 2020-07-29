6a9623fe0f977c3a6a38fb87bb9bbb99984f6267
Alexis Goodfellow <agoodfellow@wayfair.com>
2020-07-29 17:13:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# itbrowz
+
+## For all your in-terminal browsing needs
+
+This project was initially designed to just render a few pages of a website for
+D&D enthusiasts (https://5thsrd.org/) right in the terminal.
+
+Since then, it has spiraled out of control into a fledgling in-terminal web browser.
+
+#### Installation
+
+- Ensure you have python3.8, iTerm2, and git installed on your local machine
+- If you do not already have a directory for all your git repositories, run `cd ~; mkdir git_repos; cd get_repos;`
+- Clone this repository with `git clone https://github.com/AlexisGoodfellow/itbrowz.git`
+- Run `cd itbrowz; pip3 install -r requirements.txt; ln ~/git_repos/itbrowz/itbrowz.py /usr/local/bin/itbrowz`
+
+#### Using itbrowz
+
+You can only use `itbrowz` inside of iTerm2 on MacOS.
+`itbrowz` does not work in the native Mac Terminal utility.
+
+Basic usage is `itbrowz --url <desired_website_url>`.
+
+Because `itbrowz` has not yet implemented rendering for all possible HTML elements,
+you might need to narrow its scope a bit. To do this, you can supply an extra argument
+to filter down to the element it starts rendering from. This usage looks like
+
+`itbrowz --url <desired_website_url> --div <div_id_or_class>`
+
+You can determine the value for `<div_id_or_class>` from your primary graphical
+browser's Developer Tools.
+
+In keeping with its roots, `itbrowz` still provides convenience wrappers for
+accessing D&D spells, classes, and races. To do this, simply do one of:
+
+- `itbrowz --spell <spell_name>`
+- `itbrowz --class <class_name>`
+- `itbrowz --race <race_name>`
+
+#### Missing features
+
+`itbrowz` does not support JavaScript or custom CSS styling. This is intentional,
+as supporting such features is a massive undertaking and the current priority is
+to continue implementing rendering for additional HTML tags.
+
+#### Contributing
+
+Open an issue on GitHub if you experience any difficulty using `itbrowz` which
+clearly describes what the problem is and what steps you've taken to solve it so far.
---
+++ itbrowz.py
@@ -0,0 +1,385 @@
+#!/usr/bin/env python3
+import argparse
+import shutil
+from dataclasses import dataclass
+from typing import List
+from copy import deepcopy
+
+from bs4 import BeautifulSoup  # type: ignore
+
+import requests
+from termcolor import colored
+
+
+@dataclass
+class TerminalRenderData:
+    color: str
+    attrs: List[str]
+    div_depth: int
+    list_depth: int
+    span_depth: int
+    base_terminal_width: int
+
+    def __init__(self):
+        self.color = "green"
+        self.attrs = []
+        self.div_depth = 0
+        self.list_depth = 0
+        self.span_depth = 0
+        self.base_terminal_width = shutil.get_terminal_size()[0]
+
+    def consumed_width(self):
+        if self.list_depth == 0:
+            return self.div_depth * 2
+        else:
+            return (self.div_depth * 2) + (self.list_depth * 4) + 3
+
+    def avaliable_terminal_width(self):
+        return self.base_terminal_width - self.consumed_width()
+
+    def clean_strings(self, base_string):
+        cut_strings = base_string.split("\n")
+        cleaned_strings = []
+        for string in cut_strings:
+            formatted_strings = [
+                str(string)[i : i + self.avaliable_terminal_width()]
+                for i in range(0, len(string), self.avaliable_terminal_width())
+            ]
+            for s in formatted_strings:
+                cleaned_strings.append(s)
+        return cleaned_strings
+
+    def render_root_text(self, base_string):
+        if self.list_depth != 0:
+            return [
+                [
+                    colored("|" * self.div_depth, "white", attrs=[]),
+                    colored(("    " * self.list_depth) + "-- ", "magenta", attrs=[]),
+                    colored(s, self.color, attrs=self.attrs),
+                    colored(
+                        " " * (self.avaliable_terminal_width() - len(s)),
+                        self.color,
+                        attrs=[],
+                    ),
+                    colored("|" * self.div_depth, "white", attrs=[]),
+                ]
+                for s in self.clean_strings(base_string)
+            ]
+        return [
+            [
+                colored("|" * self.div_depth, "white", attrs=[]),
+                colored(s, self.color, attrs=self.attrs),
+                colored(
+                    " " * (self.avaliable_terminal_width() - len(s)),
+                    self.color,
+                    attrs=[],
+                ),
+                colored("|" * self.div_depth, "white", attrs=[]),
+            ]
+            for s in self.clean_strings(base_string)
+        ]
+
+
+def oops_i_wrote_a_browser(root_element):
+    render_html(root_element.contents)
+
+
+def spell_lookup(args):
+    res = requests.get(f"https://5thsrd.org/spellcasting/spells/{args.spell}/")
+    text = BeautifulSoup(res.content, "html.parser")
+    root = text.body.find("div", class_="container", recursive=False)
+    oops_i_wrote_a_browser(root)
+
+
+def class_lookup(args):
+    res = requests.get(f"https://5thsrd.org/character/classes/{args._class}/")
+    text = BeautifulSoup(res.content, "html.parser")
+    root = text.body.find("div", class_="container", recursive=False)
+    oops_i_wrote_a_browser(root)
+
+
+def race_lookup(args):
+    res = requests.get(f"https://5thsrd.org/character/races/{args.race}/")
+    text = BeautifulSoup(res.content, "html.parser")
+    root = text.body.find("div", class_="container", recursive=False)
+    oops_i_wrote_a_browser(root)
+
+
+def arbitrary_url_lookup(args):
+    res = requests.get(args.url)
+    text = BeautifulSoup(res.content, "html.parser")
+    if args.div:
+        root = text.body.find("div", class_=args.div, recursive=False)
+        if root is None:
+            root = text.body.find("div", id=args.div, recursive=False)
+    else:
+        root = text.body
+    oops_i_wrote_a_browser(root)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="DND Quick lookup")
+    parser.add_argument(
+        "-s", "--spell", type=str, dest="spell", default="", help="Spell"
+    )
+    parser.add_argument(
+        "-c", "--class", type=str, dest="_class", default="", help="Class"
+    )
+    parser.add_argument("-r", "--race", type=str, dest="race", default="", help="Race")
+    parser.add_argument("-u", "--url", type=str, dest="url", default="", help="Arbitrary URL")
+    parser.add_argument("-d", "--div", type=str, dest="div", default="", help="Div identifier")
+    args = parser.parse_args()
+    for k, v in vars(args).items():
+        vars(args)[k] = v.replace("-", "_") if k == "spell" else v
+        vars(args)[k] = v.replace("_", "-") if k == "race" else v
+    if args.spell:
+        spell_lookup(args)
+    elif args._class:
+        class_lookup(args)
+    elif args.race:
+        race_lookup(args)
+    elif args.url:
+        arbitrary_url_lookup(args)
+    else:
+        pass
+
+
+def element_renderer(elem, render_info):
+    # Base case - all that's left is text
+    if elem.name is None:
+        return render_info.render_root_text(elem)
+    # RECURSIVE CASES - THIS IS A DOOZY
+    # Table is not a root element or base case, but we render it like one
+    # for now because formatting tables is a royal pain in the @$$
+    elif elem.name == "table":
+        render_table(elem, render_info)
+        return []  # THIS IS A HACK
+    elif elem.name == "ol" or elem.name == "ul":
+        return render_list(elem, render_info)
+    elif (
+        elem.name == "div"
+        or elem.name == "center"
+        or elem.name == "header"
+        or elem.name == "footer"
+    ):
+        return render_div(elem, render_info)
+    elif elem.name == "span":
+        return render_span(elem, render_info)
+    elif elem.name == "em" or elem.name == "i":
+        r = deepcopy(render_info)
+        r.attrs += ["bold"]
+        return [element_renderer(x, r) for x in elem.contents]
+    elif elem.name == "strong" or elem.name == "b":
+        r = deepcopy(render_info)
+        r.attrs += ["underline"]
+        return [element_renderer(x, r) for x in elem.contents]
+    elif elem.name == "hr":
+        return render_horizontal_line(render_info)
+    elif elem.name == "p" or elem.name == "button":
+        r = deepcopy(render_info)
+        r.attrs = []
+        return [element_renderer(x, r) for x in elem.contents]
+    elif elem.name == "li":
+        r = deepcopy(render_info)
+        r.attrs = []
+        r.color = "magenta"
+        return [element_renderer(x, r) for x in elem.contents]
+    elif elem.name == "a":
+        return render_link(elem, render_info)
+    elif elem.name == "blockquote":
+        r = deepcopy(render_info)
+        r.attrs = []
+        r.color = "cyan"
+        return [element_renderer(x, r) for x in elem.contents]
+    elif elem.name in ["h1", "h2", "h3", "h4", "h5", "h6"]:
+        r = deepcopy(render_info)
+        r.attrs += ["reverse"]
+        r.color = "red"
+        return [element_renderer(x, r) for x in elem.contents]
+    elif (
+        elem.name == "ins"
+        or elem.name == "br"
+        or elem.name == "wbr"
+        or elem.name == "script"
+    ):
+        return []  # Will be flattened out
+    else:
+        raise NotImplementedError(f"{elem.name} not implemented yet")
+
+
+def flatten(l):
+    flattened = [item for sublist in l for item in sublist]
+    if any([isinstance(x, list) for x in flattened]):
+        return flatten(flattened)
+    else:
+        return flattened
+
+
+def render_html(elements):
+    # Collect child elements together into a list
+    render_info = TerminalRenderData()
+    for x in elements:
+        all_child_elements = flatten(element_renderer(x, render_info))
+        for y in all_child_elements:
+            eprint(y)
+        eprint("\n")
+
+
+# TABLES AND LISTS ARE HARD AS NAILS TO FORMAT RIGHT - THESE ARE HACKS
+def render_list(list_, render_info):
+    r = deepcopy(render_info)
+    r.color = "magenta"
+    r.list_depth += 1
+    return [element_renderer(x, r) for x in list_.contents]
+
+
+def render_horizontal_line(render_info):
+    return [
+        colored(
+            "|" * render_info.div_depth
+            + ("    " * render_info.list_depth)
+            + ("-" * render_info.avaliable_terminal_width())
+            + "|" * render_info.div_depth,
+            "cyan",
+            attrs=[],
+        )
+    ]
+    pass
+
+
+def render_link(link, render_info):
+    return [element_renderer(x, render_info) for x in link.contents]
+
+
+def render_div(div, render_info):
+    border_color = "white"
+    r = deepcopy(render_info)
+    r.div_depth += 1
+    div_attr_keys = [k for k in div.attrs.keys()]
+    if "id" in div_attr_keys:
+        div_title = div["id"]
+    elif "class" in div_attr_keys:
+        div_title = " ".join(div["class"])
+    else:
+        div_title = ""
+    return [
+        [
+            colored("|" * render_info.div_depth, border_color, attrs=[]),
+            colored("/", border_color, attrs=[]),
+            colored(
+                div_title
+                + (
+                    "-"
+                    * (
+                        shutil.get_terminal_size().columns
+                        - (2 * r.div_depth)
+                        - len(div_title)
+                    )
+                ),
+                border_color,
+                attrs=[],
+            ),
+            colored("\\", border_color, attrs=[]),
+            colored("|" * render_info.div_depth, border_color, attrs=[]),
+        ],
+        [element_renderer(x, r) for x in div.contents if str(x) != "\n"],
+        [
+            colored("|" * render_info.div_depth, border_color, attrs=[]),
+            colored("\\", border_color, attrs=[]),
+            colored(
+                "~"
+                + div_title
+                + (
+                    "-"
+                    * (
+                        shutil.get_terminal_size().columns
+                        - (2 * r.div_depth)
+                        - len(div_title)
+                        - 1
+                    )
+                ),
+                border_color,
+                attrs=[],
+            ),
+            colored("/", border_color, attrs=[]),
+            colored("|" * render_info.div_depth, border_color, attrs=[]),
+        ]
+    ]
+
+
+# This will throw off alignments - thinking about a fix
+def render_span(span, render_info):
+    r = deepcopy(render_info)
+    return [
+        [
+            colored("[", "blue", attrs=[]),
+            element_renderer(x, r),
+            colored("]", "blue", attrs=[]),
+        ]
+        for x in span.contents if str(x) != "\n"
+    ]
+
+
+# HACK HACK HACK
+def render_table(table, render_info):
+    head = table.find("thead")
+    head_rows = head.find("tr")
+    row_head_data = [x.get_text() for x in head_rows.find_all("th")]
+    body = table.find("tbody")
+    body_rows = body.find_all("tr")
+    body_data = [[x.get_text() for x in row.find_all("td")] for row in body_rows]
+    render_table_helper(row_head_data, body_data, render_info)
+
+
+def render_table_helper(row_head_data, body_data, render_info):
+    row_head_lengths = [len(x) for x in row_head_data]
+    max_lengths = []
+    for row_elems in range(0, len(body_data[0])):
+        max_lengths.append(
+            max([len(body_data[rows][row_elems]) for rows in range(0, len(body_data))])
+        )
+    maximum_lengths = []
+    for i in range(0, len(row_head_lengths)):
+        max_length = (
+            max_lengths[i]
+            if max_lengths[i] > row_head_lengths[i]
+            else row_head_lengths[i]
+        )
+        maximum_lengths.append(max_length)
+    print_table_format_line(maximum_lengths, render_info)
+    print_table_data_line(maximum_lengths, row_head_data, render_info)
+    print_table_format_line(maximum_lengths, render_info)
+    for row in body_data:
+        print_table_data_line(maximum_lengths, row, render_info)
+    print_table_format_line(maximum_lengths, render_info)
+
+
+def print_table_format_line(maximum_lengths, render_info):
+    eprint(colored("+", "yellow"))
+    for length in maximum_lengths:
+        eprint(colored("-", "yellow"))
+        for x in range(0, length):
+            eprint(colored("-", "yellow"))
+        eprint(colored("-+", "yellow"))
+    eprint(colored("\n", "yellow"))
+
+
+def print_table_data_line(maximum_lengths, row_head_data, render_info):
+    eprint(colored("|", "yellow"))
+    for i in range(0, len(row_head_data)):
+        eprint(colored(" ", "yellow"))
+        padding_amount = maximum_lengths[i] - len(row_head_data[i])
+        padding = ""
+        for j in range(0, padding_amount):
+            padding += " "
+        eprint(colored(row_head_data[i] + padding, "yellow"))
+        eprint(colored(" |", "yellow"))
+    eprint(colored("\n", "yellow"))
+
+
+def eprint(arg):
+    print(arg, end="")
+
+
+if __name__ == "__main__":
+    main()
